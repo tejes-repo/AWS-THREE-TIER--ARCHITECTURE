63610fb7a298419dabc39e8b9feddc5ae3765ea8
P TEJESWAR REDDY <tejeswarreddy00@gmail.com>
2026-09-17 22:00:43 +0530

    Delete 3tier-app-deployment-aws-main directory

--- 3tier-app-deployment-aws-main/application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'my3tierdb.chokigows00i.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'root123456',
-    DB_DATABASE : 'webappdb'
-});
