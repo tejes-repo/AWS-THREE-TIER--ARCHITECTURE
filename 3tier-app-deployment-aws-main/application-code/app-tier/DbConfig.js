7f2fcbb92296d6ffa597a98212b398d08faeb3dd
P TEJESWAR REDDY <tejeswarreddy00@gmail.com>
2026-09-17 21:58:57 +0530

    Add files via upload

---
+++ 3tier-app-deployment-aws-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.chokigows00i.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root123456',
+    DB_DATABASE : 'webappdb'
+});
